bd901c776b644ca0b9022062d7826c7ab0b80e61
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-04-12 14:37:16 +0100

    added functionality to keep player within boundaries

--- src/GAME.c
+++ src/GAME.c
@@ -4,6 +4,43 @@
 #include "GAME.h"
 #include "UserInterface.h"
 #include <stdlib.h>
+
+bool checkValidUp(GameState *state) {
+    if (state->y < 0 || (state->y == 0 && (state->x == 1 || state->x == 6)) ||
+        (state->y == 1 && (state->x == 0 || state->x == 7))) {
+        state->y++;
+        return false;
+    }
+    return true;
+}
+
+bool checkValidDown(GameState *state) {
+    if (state->y > 7 || (state->y == 7 && (state->x == 1 || state->x == 6)) ||
+        (state->y == 6 && (state->x == 0 || state->x == 7))) {
+        state->y--;
+        return false;
+    }
+    return true;
+}
+
+bool checkValidRight(GameState *state) {
+    if (state->x > 7 || (state->x == 7 && (state->y == 1 || state->y == 6)) ||
+        (state->x == 6 && (state->y == 0 || state->y == 7))) {
+        state->x--;
+        return false;
+    }
+    return true;
+}
+
+bool checkValidLeft(GameState *state) {
+    if (state->x < 0 || (state->x == 0 && (state->y == 1 || state->y == 6)) ||
+        (state->x == 1 && (state->y == 0 || state->y == 7))) {
+        state->x++;
+        return false;
+    }
+    return true;
+}
+
 void run_game(game *game) {
     int maxScreenX = getmaxx(stdscr);
     WINDOW *boardWin = newwin(BOARD_HEIGHT, BOARD_WIDTH, LOGO_HEIGHT + 1, maxScreenX / 2 - BOARD_WIDTH / 2);
@@ -12,36 +49,47 @@ void run_game(game *game) {
     refresh();
 
     GameState state;
-    state.x = 4;
-    state.y = 4;
-    state.player1Top = 18;
-    state.player2Top = 18;
+    state.x = state.y = 4;
+    state.player1Top = state.player2Top = 18;
+
     drawBoard(boardWin, game->board, state);
     drawStack(stackWin, &game->board[state.y][state.x]);
     do {
         switch (getch()) {
             case KEY_UP:
                 state.y--;
-                drawBoard(boardWin, game->board, state);
-                drawStack(stackWin, &game->board[state.y][state.x]);
+                if (checkValidUp(&state)) {
+                    drawBoard(boardWin, game->board, state);
+                    drawStack(stackWin, &game->board[state.y][state.x]);
+                }
+                printw("x:%d y:%d", state.x, state.y);
                 break;
             case KEY_DOWN:
                 state.y++;
-                drawBoard(boardWin, game->board, state);;
-                drawStack(stackWin, &game->board[state.y][state.x]);
+                if (checkValidDown(&state)) {
+                    drawBoard(boardWin, game->board, state);
+                    drawStack(stackWin, &game->board[state.y][state.x]);
+                }
                 break;
             case KEY_LEFT:
                 state.x--;
-                drawBoard(boardWin, game->board, state);
-                drawStack(stackWin, &game->board[state.y][state.x]);
+                if (checkValidLeft(&state)) {
+                    drawBoard(boardWin, game->board, state);
+                    drawStack(stackWin, &game->board[state.y][state.x]);
+                }
                 break;
             case KEY_RIGHT:
                 state.x++;
-                drawBoard(boardWin, game->board, state);
-                drawStack(stackWin, &game->board[state.y][state.x]);
+                if (checkValidRight(&state)) {
+                    drawBoard(boardWin, game->board, state);
+                    drawStack(stackWin, &game->board[state.y][state.x]);
+                }
                 break;
-            case KEY_ENTER  :
-                state.x++;
+            case 'f'  :
+                state.selectedX = state.x;
+                state.selectedY = state.y;
+                state.selected = true;
+                drawBoard(boardWin, game->board, state);
                 break;
             default:
                 break;
