d7ba0b67e91d774cd516b384138a39e9655a632d
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-04-13 17:26:17 +0100

    Added function to trim pieces down to 5

--- src/GAME.c
+++ src/GAME.c
@@ -60,7 +60,35 @@ bool checkOwnedPlayer(Game *game, GameState *state) {
 bool validMove(square board[BOARD_SIZE][BOARD_SIZE], GameState state) {
     int height = board[state.selectedY][state.selectedX].height;
     int numOfMoves = abs(state.selectedX - state.x) + abs(state.selectedY - state.y);
+    /*TODO: Output message to a box to state that the move is not valid*/
     return height >= numOfMoves;
+}
+
+//Trims piece down to a height of 5
+void trimStack(square *s, Player *player) {
+    /*Only trims pieces down to a height of 5*/
+    if (s->height > 5) {
+        /*Creates a pointer that skips directly to the 5th node*/
+        piece_node *curNode = s->head->next->next->next->next->next;
+        /*Loops through and trims the stack accordingly*/
+        while (curNode != NULL) {
+            /*Temporary pointer so that we can free it later*/
+            piece_node *temp = curNode;
+            /*Increments the current pointer*/
+            curNode = curNode->next;
+            /*If the player owns the piece, we increment their retained pieces(To be used for graveyard functionality)*/
+            if (temp->colour == player->colour) {
+                player->retainedPieces++;
+                free(temp);
+            } else {
+                player->capturedPieces++;
+                free(temp);
+            }
+        }
+        //Ends our new stack with a null and sets the new height to 5
+        s->head->next->next->next->next->next = NULL;
+        s->height = 5;
+    }
 }
 
 void run_game(Game *game) {
@@ -68,7 +96,7 @@ void run_game(Game *game) {
     state.x = state.y = 4;
     state.player1Top = state.player2Top = 18;
     state.playerTurn = 0;
-
+    
     drawBoard(game->board, state);
     drawStack(&game->board[state.y][state.x]);
     do {
@@ -103,14 +131,12 @@ void run_game(Game *game) {
                 break;
             case 'f'  :
                 if (state.selected == true) {
-                    //TODO: Check that the move is valid (Use coordinates to do so)
                     if (validMove(game->board, state)) {
                         printw("valid");
                         //TODO: move piece if it's valid,
-                        //TODO: alter number of player tops
+                        //TODO: alter number of player tops, update number of player captures etc.
                         //TODO: change player turn
-                    }
-                    else
+                    } else
                         printw("not valid");
                 } else {
                     /*Checks if the piece is owned by the current player
@@ -119,12 +145,20 @@ void run_game(Game *game) {
                         drawBoard(game->board, state);
                     }
                 }
+            case 'g':
+                trimStack(&game->board[state.y][state.x], &game->player[state.playerTurn]);
+                printw("Num of Pieces retained: %d Num Of pieces captured %d",
+                       game->player[state.playerTurn].retainedPieces, game->player[state.playerTurn].capturedPieces);
+                drawStack(&game->board[state.y][state.x]);
                 break;
-                //TODO: Build Graveyard and give the ability to move pieces from the graveyard
+                //TODO: Build Graveyard and give the ability to move pieces from the graveyard.
+                // Suggestion: Add to the game state a counter to keep track of the number of pieces a player has in the graveyard
+                // Remember to keep stack of size 5
             default:
                 break;
         }
-    } while (state.player2Top != 0);
+    } while (state.player2Top !=
+             0);//TODO: Implement wincondition, where graveyard pieces are 0 and they have no pieces they can move on the board
 
     delwin(boardWin);
     delwin(stackWin);
