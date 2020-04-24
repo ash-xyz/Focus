983cce3c4692d442752f506f77fe039dc63749b2
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-04-24 16:57:42 +0100

    Added comments

--- src/GAME.c
+++ src/GAME.c
@@ -7,6 +7,7 @@
 
 /*Checks if an upward move is valid*/
 bool checkValidUp(GameState *state) {
+    /*If the move is out of bounds it returns false*/
     if (state->y < 0 || (state->y == 0 && (state->x == 1 || state->x == 6)) ||
         (state->y == 1 && (state->x == 0 || state->x == 7))) {
         state->y++;
@@ -16,6 +17,7 @@ bool checkValidUp(GameState *state) {
 }
 /*Checks if a downward move is valid*/
 bool checkValidDown(GameState *state) {
+    /*If the move is out of bounds it returns false*/
     if (state->y > 7 || (state->y == 7 && (state->x == 1 || state->x == 6)) ||
         (state->y == 6 && (state->x == 0 || state->x == 7))) {
         state->y--;
@@ -25,6 +27,7 @@ bool checkValidDown(GameState *state) {
 }
 /*Checks if a rightward move is valid*/
 bool checkValidRight(GameState *state) {
+    /*If the move is out of bounds it returns false*/
     if (state->x > 7 || (state->x == 7 && (state->y == 1 || state->y == 6)) ||
         (state->x == 6 && (state->y == 0 || state->y == 7))) {
         state->x--;
@@ -35,6 +38,7 @@ bool checkValidRight(GameState *state) {
 
 /*Checks if a leftward move is valid*/
 bool checkValidLeft(GameState *state) {
+    /*If the move is out of bounds it returns false*/
     if (state->x < 0 || (state->x == 0 && (state->y == 1 || state->y == 6)) ||
         (state->x == 1 && (state->y == 0 || state->y == 7))) {
         state->x++;
@@ -45,16 +49,19 @@ bool checkValidLeft(GameState *state) {
 
 /*Checks if a selected piece is owned by the player*/
 bool checkOwnedPlayer(Game *game, GameState *state) {
+    /*Checks if the square contains any pieces*/
     if (game->board[state->y][state->x].head == NULL) {
         displayMessage("You can't select a square with no pieces");
         return false;
     }
+    /*If the player owns the piece, it is then marked as selected*/
     if (game->board[state->y][state->x].head->colour == game->player[state->playerTurn].colour) {
         state->selected = true;
         state->selectedX = state->x;
         state->selectedY = state->y;
         return true;
     }
+    /*If the player doesn't control the square:*/
     displayMessage("You can't select a piece that isn't yours!");
     return false;
 }
@@ -62,8 +69,10 @@ bool checkOwnedPlayer(Game *game, GameState *state) {
 /*Checks if a piece move is valid(within bounds)*/
 bool validMove(square board[BOARD_SIZE][BOARD_SIZE], GameState state) {
     /*Ensures that you are within bounds*/
-    int height = board[state.selectedY][state.selectedX].height;
-    int numOfMoves = abs(state.selectedX - state.x) + abs(state.selectedY - state.y);
+    int height = board[state.selectedY][state.selectedX].height;//Gets the height of the selected square
+    int numOfMoves = abs(state.selectedX - state.x) + abs(state.selectedY -
+                                                          state.y);//Retrieves the distance between the selected square and current square
+    /*If the height of the selected square is less than the distance between the two squares, we can't move*/
     if (height < numOfMoves) {
         displayMessage("You can't move that far!");
     }
@@ -166,7 +175,7 @@ bool resurrectPiece(Game *game, GameState *state) {
     printw("Moves %d %d", state->moveablePieces[0], state->moveablePieces[1]);
     /*Check if you have any graveyard pieces*/
     if (game->player[state->playerTurn].graveyardPieces > 0) {
-        /*Initialises a piece of the players color*/
+        /*Initialises a piece of the players colour*/
         piece_node *resurrectedPiece = (piece_node *) malloc(sizeof(piece_node));
         resurrectedPiece->colour = game->player[state->playerTurn].colour;
 
@@ -201,18 +210,23 @@ bool continueGame(Game *game, GameState state) {
 }
 
 void run_game(Game *game) {
+    /*Initialises a variable to store the current state of our game */
     GameState state;
-    state.x = state.y = 4;
-    state.playerTurn = 0;
-    state.moveablePieces[0] = state.moveablePieces[1] = 18; //Initialises the number of moveable pieces for each player
+    state.x = state.y = 4; //Stores where the player is currently at
+    state.playerTurn = 0; //Stores the player turn, 0 for player 1, 1 for player 2
+    state.moveablePieces[0] = state.moveablePieces[1] = 18; //Stores the number of moveable pieces each player has
 
+    /*Initially draws our UI*/
     drawBoard(game->board, state);
     drawStack(&game->board[state.y][state.x]);
     displayPlayer(game->player[state.playerTurn]);
+    /*Loop that runs our game until it has terminated*/
     do {
         state.moveMade = false;//Tells us whether a player has made a move, helps avoid computing the win condition a needless amount of times
         bool redraw = false;//Tells us whether or not to redraw the board and screen
+        /*Gets input from the keyboard*/
         switch (getch()) {
+            /*Acts on player movement in a given direction*/
             case KEY_UP:
                 state.y--;
                 redraw = checkValidUp(&state);
@@ -229,24 +243,31 @@ void run_game(Game *game) {
                 state.x++;
                 redraw = checkValidRight(&state);
                 break;
+                /*Button to select, unselect and place  a piece down*/
             case 'f'  :
                 if (state.selected == true) {
-                    /*Unselects a piece*/
+                    /*Unselect functionality - if you select a selected piece, it is unselected*/
                     if (state.x == state.selectedX && state.y == state.selectedY) {
                         state.selected = false;
                         drawBoard(game->board, state);
-                    } else if (validMove(game->board, state)) {
+                    }
+                        /*Checks whether placing a piece down is valid*/
+                    else if (validMove(game->board, state)) {
+                        /*IF it's valid it moves the stack to said the current square*/
                         movePieces(game, &state);
                         updateGameState(&state, game);//Updates the current game state, used to switch players
                         redraw = true;
                     }
-                } else {
+                }
+                    /*Selects a square*/
+                else {
                     /*Checks if the selected piece is owned by the current player*/
                     if (checkOwnedPlayer(game, &state) == true) {
                         drawBoard(game->board, state);
                     }
                 }
                 break;
+                /*Graveyard Functionality*/
             case 'g':
                 /*Resurrects a piece from a players graveyard*/
                 if (resurrectPiece(game, &state)) {
@@ -254,6 +275,7 @@ void run_game(Game *game) {
                     redraw = true;
                 }
                 break;
+                /*Help button - Prints rules and instructions*/
             case 'h':
                 displayRules();
                 displayPlayer(game->player[state.playerTurn]);
@@ -261,6 +283,7 @@ void run_game(Game *game) {
             default:
                 break;
         }
+        /*Redraws the UI upon a valid key press*/
         if (redraw) {
             drawBoard(game->board, state);
             drawStack(&game->board[state.y][state.x]);
@@ -271,8 +294,8 @@ void run_game(Game *game) {
 
     /*Deletes all our previously created windows*/
     deleteWindows();
-
+    /*Draws the winner alongside their current info*/
     drawWinner(game->player[state.playerTurn]);//Draws a screen with our winner
-
+    /*Ends ncurses screen*/
     endwin();
 }
